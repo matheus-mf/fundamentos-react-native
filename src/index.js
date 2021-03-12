2fc36e6a98497b7ec35b9c9ebe803651988ca834
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-12 19:00:15 -0300

    feat: creating new projects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text } from 'react-native';
+import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -12,6 +12,17 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${ Date.now() }`,
+      owner: 'Matheus Moreira',
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -22,6 +33,14 @@ export default function App() {
           renderItem={ ({ item: project }) => (
             <Text style={ style.project }>{ project.title }</Text>
           ) } />
+
+        <TouchableOpacity
+          activeOpacity={ 0.6 }
+          style={ style.button }
+          onPress={ handleAddProject }
+        >
+          <Text style={ style.buttonText }>Adicionar Projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -35,5 +54,17 @@ const style = StyleSheet.create({
   project: {
     color: '#fff',
     fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
